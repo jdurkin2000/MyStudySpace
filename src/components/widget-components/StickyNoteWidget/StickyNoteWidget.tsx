be977a6f0f6944adf414978b5de2c56ef860895b
jdurkin2000 <durkin.jfd@gmail.com>
2025-03-21 15:51:09 -0400

    Added fontawesome library to simplify current and future ui stuff

--- src/components/widget-components/StickyNoteWidget/StickyNoteWidget.tsx
+++ src/components/widget-components/StickyNoteWidget/StickyNoteWidget.tsx
@@ -1,10 +1,45 @@
 import WidgetBase, { WidgetBaseProps } from "components/WidgetBase";
-import styles from "./StickyNoteWidget.module.css"
+import styles from "./StickyNoteWidget.module.css";
+import { useState } from "react";
 
 export function StickyNoteWidget(props: WidgetBaseProps) {
+  const [isUnderline, setIsUnderline] = useState(false);
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [bgColor, setBgColor] = useState("#ffa723");
+
   return (
     <WidgetBase className={`${styles.stickyNote}`} {...props}>
-      <textarea  className={`focus-visible:outline-none h-40 ${styles.textarea}`} maxLength={Number.MAX_SAFE_INTEGER}/>
+      <textarea
+        className={`focus-visible:outline-none h-40 ${styles.textarea}`}
+        maxLength={Number.MAX_SAFE_INTEGER}
+      />
+
+      <div className={`${styles.options}`}>
+        <label className={`${styles.container}`}>
+      
+          <input type="checkbox" />
+          <span className={`${styles.checkmark}`}><i class=""></i></span>
+        </label>
+
+        <label className={`${styles.container}`}>
+        
+          <input type="checkbox" />
+          <span className={`${styles.checkmark}`}></span>
+        </label>
+
+        <label className={`${styles.container}`}>
+      
+          <input type="checkbox" />
+          <span className={`${styles.checkmark}`}></span>
+        </label>
+
+        <label className={`${styles.container}`}>
+       
+          <input type="checkbox" />
+          <span className={`${styles.checkmark}`}></span>
+        </label>
+      </div>
     </WidgetBase>
   );
 }
