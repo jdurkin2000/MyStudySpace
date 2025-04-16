6521c2e50681057256f1f243dc4273f69a893b79
Maulik Durani <md00211@uga.edu>
2025-04-16 13:51:18 -0400

    messing around with custom css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ export default function HomePage() {
       <Navbar state={false} /> 
 
       {/* Main Section */}
-      <main className="p-8 flex flex-1 flex-col md:flex-row gap-8 justify-center items-center bg-green-600">
+      <main className="p-8 flex flex-1 flex-col md:flex-row gap-8 justify-center items-center bg-[#fbf5fe]">
         {/* GIF Display */}
-        <div className="text-center w-80 flex h-80 justify-center items-center bg-gray-300 text-gray-600">
+        <div className="text-center w-80 flex h-80 justify-center items-center bg- text-gray-600">
           <Image
             src="/cat-spinning.gif"
             alt="App preview coming soon"
@@ -25,22 +25,22 @@ export default function HomePage() {
         </div>
 
         {/* Features List */}
-        <section className="max-w-md flex flex-col gap-6 text-white">
+        <section className="max-w-md flex flex-col gap-6 text-[#f0ac4b]">
           <div>
             <h2 className="font-bold mb-2">Tailor Your Study Sessions</h2>
-            <p className="rounded bg-green-300 p-3 text-green-900">
+            <p className="p-3 bg-[#f48d77] rounded text-black">
               Customize your workspace with widgets built to boost your focus.
             </p>
           </div>
           <div>
             <h2 className="mb-2 font-bold">Tired of Juggling Tabs?</h2>
-            <p className="p-3 text-green-900 rounded bg-green-300">
+            <p className="p-3 bg-[#f48d77] rounded text-black">
               Find everything you need in one place, powered by smart tools.
             </p>
           </div>
           <div>
             <h2 className="mb-2 font-bold">Zero Ads, Zero Distractions</h2>
-            <p className="bg-green-300 p-3 text-green-900 rounded">
+            <p className="p-3 bg-[#f48d77] rounded text-black">
               We’re built for students, not ad revenue — goodbye Quizlet
               clutter.
             </p>
